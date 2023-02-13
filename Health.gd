aedccc211a060b26aa8588baa1d347ccb3f8669b
Kristoffer <kristoffer.mn@hotmail.no>
2023-02-13 10:07:43 +0100

    Health - Power_Ups spawn

--- Health.gd
+++ Health.gd
@@ -4,21 +4,21 @@ const UP = Vector2(0, -1)
 
 
 var screen_size = get_viewport_rect().size
-var player = get_parent().get_node("Player1")
+#var player = get_parent().get_node("Player1")
+
+ 
+
+
 func _ready():
 	pass
-
+	
 	
 func _physics_process(delta): # delta er standard for 60 FPS
+	pass
+	#beveg nedover (position.y)
 	
 	
-	
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-	
-	
-
-
 func _on_Health_up_body_entered(body):
 	body.health = body.max_health 
-	
+	print(body.health)
+	queue_free()
--- world_test_delete.gd
+++ world_test_delete.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 
 var Laser = preload("res://LaserTag.tscn")
-
+var Health = preload("res://Health.tscn")
 
 func _on_Player1_spawn_laser(location):
 	var l = Laser.instance()
 	l.global_position = location
 	add_child(l)
 	
+
+
+
+func _on_Power_Ups_spawn_timeout():
+	var health_instance = Health.instance()
+	health_instance.global_position = Vector2(100, 400)#Gjør denne Vectoren random (skal komme nedover)
+	add_child(health_instance)
